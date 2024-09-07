12487d996d35f2fce1160980cc3ff4c1bbac31d3
Udodirim L. Agonsi <88558628+Agonsi123@users.noreply.github.com>
2024-09-07 18:52:10 +0100

    Update Skills.jsx

--- src/components/forms/Skills.jsx
+++ src/components/forms/Skills.jsx
@@ -24,23 +24,23 @@ const Skills = () => {
         <Navbar />
         <div className='w-full bg-white flex items-center shadow-lg p-8 md:max-w-lg md:mx-auto'>
             <form onSubmit={handleSubmit} className="mb-4 md:flex md:flex-wrap md:justify-between">
-                <div className='flex flex-col mb-4 w-[18.6rem] md:w-1/2'>
+                <div className='flex flex-col mb-4 w-[19.6rem] md:w-1/2'>
                     <label className='mb-2 font-bold md:text-lg text-grey-darkest'>Academic Skills</label>
                     <textarea className='border py-6 px-3 text-[12px] text-grey-darkest' name='academicSkills' value={formData.academicSkills} onChange={handleChange} placeholder='List your skills e.g HTML' />
                 </div>
-                <div className='flex flex-col mb-4 w-[18.6rem] md:w-1/2 md:ml-[15.1rem] md:mt-[-8.7rem]'>
+                <div className='flex flex-col mb-4 w-[19.6rem] md:w-1/2 md:ml-[15.1rem] md:mt-[-8.7rem]'>
                     <label className='mb-2 font-bold md:text-lg text-grey-darkest'>Non-Academic Skills</label>
                     <textarea className='border py-6 px-3 text-[12px] text-grey-darkest' name='nonacademicSkills' value={formData.nonacademicSkills} onChange={handleChange} placeholder='e.g communication skills' />
                 </div>
-                <div className='flex flex-col mb-4 w-[18.6rem] md:w-1/2'>
+                <div className='flex flex-col mb-4 w-[19.6rem] md:w-1/2'>
                     <label className='mb-2 font-bold md:text-lg text-grey-darkest'>Certifications</label>
                     <textarea className='border py-6 px-3 text-[12px] text-grey-darkest' name='certificates' value={formData.certificates} onChange={handleChange} placeholder='Enter trainings' />
                 </div>
-                <div className='flex flex-col mb-4 w-[18.6rem] md:w-1/2 md:ml-[15.1rem] md:mt-[-8.7rem]'>
+                <div className='flex flex-col mb-4 w-[19.6rem] md:w-1/2 md:ml-[15.1rem] md:mt-[-8.7rem]'>
                     <label className='mb-2 font-bold md:text-lg text-grey-darkest'>Tools</label>
                     <textarea className='border py-6 px-3 text-[12px] text-grey-darkest' name='tools' value={formData.tools} onChange={handleChange} placeholder='Enter your tools e.g Figma' />
                 </div>
-                <button className='bg-gradient-to-r from-[#23e27c] to-[#1369b9] hover:bg-[#1369b9] text-white mb-4 w-[18.6rem] rounded-lg md:w-[12.7rem] py-2' type="submit">Save</button>
+                <button className='bg-gradient-to-r from-[#23e27c] to-[#1369b9] hover:bg-[#1369b9] text-white mb-4 w-[19.6rem] rounded-lg md:w-[12.7rem] py-2' type="submit">Save</button>
                 
             </form>
         </div>

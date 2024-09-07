a8fd446c73045df3c001eabbf926d76bc394979e
Udodirim L. Agonsi <88558628+Agonsi123@users.noreply.github.com>
2024-09-07 17:31:00 +0100

    Update PersonalInfo.jsx

--- src/components/forms/PersonalInfo.jsx
+++ src/components/forms/PersonalInfo.jsx
@@ -33,37 +33,37 @@ const PersonalInfo = () => {
                         <input className='border py-2 px-3 text-[12px] text-grey-darkest' type="text" name='name' value={formData.name} onChange={handleChange}
                     placeholder='e.g John Doe' />
                     </div>
-                    <div className='flex flex-col mb-4 w-full md:w-1/2 md:ml-[15.1rem] md:mt-[-5.5rem]'>
+                    <div className='flex flex-col mb-4 w-[18.6rem] md:w-1/2 md:ml-[15.1rem] md:mt-[-5.5rem]'>
                         <label className='mb-2 font-bold md:text-lg text-grey-darkest'>Title</label>
                         <input className='border py-2 px-3 text-[12px] text-grey-darkest ' type="text" name='title' value={formData.title} onChange={handleChange}
                     placeholder='e.g Frontend Developer' />
                     </div>
-                    <div className='flex flex-col mb-4 w-full md:w-1/2'>
+                    <div className='flex flex-col mb-4 w-[18.6rem] md:w-1/2'>
                         <label className='mb-2 font-bold md:text-lg text-grey-darkest'>GitHub Link</label>
                         <input className='border py-2 px-3 text-[12px] text-grey-darkest' type="url" name='link' value={formData.link} onChange={handleChange}
                     placeholder='Enter your GitHub profile link' />
                     </div>
-                    <div className='flex flex-col mb-4 w-full md:w-1/2 md:ml-[15.1rem] md:mt-[-5.5rem]'>
+                    <div className='flex flex-col mb-4 w-[18.6rem] md:w-1/2 md:ml-[15.1rem] md:mt-[-5.5rem]'>
                         <label className='mb-2 font-bold md:text-lg text-grey-darkest'>LinkedIn Link</label>
                         <input className='border py-2 px-3 text-[12px] text-grey-darkest' type="url" name='linkedIn' value={formData.linkedIn} onChange={handleChange}
                     placeholder='Enter your LinkedIn profile link' />
                     </div>
-                    <div className='flex flex-col mb-4 w-full md:w-1/2'>
+                    <div className='flex flex-col mb-4 w-[18.6rem] md:w-1/2'>
                         <label className='mb-2 font-bold md:text-lg text-grey-darkest'>Email</label>
                         <input className='border py-2 px-3 text-[12px] text-grey-darkest' type="email" name='email' value={formData.email} onChange={handleChange}
                     placeholder='Enter your email' />
                     </div>
-                    <div className='flex flex-col mb-4 w-full md:w-1/2 md:ml-[15.1rem] md:mt-[-5.5rem]'>
+                    <div className='flex flex-col mb-4 w-[18.6rem] md:w-1/2 md:ml-[15.1rem] md:mt-[-5.5rem]'>
                         <label className='mb-2 font-bold md:text-lg text-grey-darkest'>Phone Number</label>
                         <input className='border py-2 px-3 text-[12px] text-grey-darkest' type="tel" name='phoneNumber' value={formData.phoneNumber} onChange={handleChange}
                     placeholder='Enter your phone number' />
                     </div>
-                    <div className='flex flex-col mb-4 w-full md:w-1/2'>
+                    <div className='flex flex-col mb-4 w-[18.6rem] md:w-1/2'>
                         <label className='mb-2 font-bold md:text-lg text-grey-darkest'>Professional Summary</label>
                         <textarea className='border py-6 px-3 text-[12px] text-grey-darkest' name='summary' value={formData.summary} onChange={handleChange}
                     placeholder='Enter professionl summary' />
                     </div>
-                    <div className='flex flex-col mb-4 w-full md:w-1/2 md:ml-[15.1rem] md:mt-[-8.7rem]'>
+                    <div className='flex flex-col mb-4 w-[18.6rem] md:w-1/2 md:ml-[15.1rem] md:mt-[-8.7rem]'>
                         <label className='mb-2 font-bold md:text-lg text-grey-darkest'>Career Objective</label>
                         <textarea className='border py-6 px-3 text-[12px] text-grey-darkest' name='objectives' value={formData.objectives} onChange={handleChange}
                     placeholder='Enter goals & objectives' />

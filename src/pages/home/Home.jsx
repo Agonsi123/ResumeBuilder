dd26805acb63e9933487d81706141a35e166e975
Udodirim L. Agonsi <88558628+Agonsi123@users.noreply.github.com>
2025-09-27 15:34:40 +0100

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Home.css'
 import { useNavigate } from 'react-router-dom'
-import Button from '../../components/Button'
 
 
 const Home = () => {
@@ -17,10 +16,9 @@ const Home = () => {
           <h3>It is free</h3>
 
           <div className=''>
-            <Button text='Start Building' 
-            className="btn"
-            onClick={handleStart} 
-            />
+            <button 
+            onClick={handleStart}
+            className='bg-gradient-to-r from-[#23e27c] to-[#1369b9] hover:bg-[#1369b9] text-white md:text-[1.8rem] mb-4 w-[18.6rem] rounded-lg w-full md:max-w-[15rem] py-4 mt-4' type="submit">Start building</button>
           </div>
         </div>
     </div>
